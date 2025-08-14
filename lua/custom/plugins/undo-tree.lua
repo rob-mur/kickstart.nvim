5f960ebd23f568c6709d6efb48fdc6be615fc10b
Rob Murphy <robert.murphy@descartesunderwriting.com>
2025-08-14 14:39:18 +0200

    feat: added undo tree

---
+++ lua/custom/plugins/undo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  'mbbill/undotree',
+  config = function()
+    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    vim.opt.undofile = true
+    local undodir = vim.fn.expand '~/.undodir'
+    if vim.fn.isdirectory(undodir) == 0 then
+      vim.fn.mkdir(undodir, 'p', 0700)
+    end
+    vim.opt.undodir = undodir
+  end,
+}

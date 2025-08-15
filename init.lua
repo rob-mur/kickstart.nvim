89ab3f82cd988ee81e8474db357687db95dfb5c2
Rob Murphy <rmurphyswimmer@gmail.com>
2025-08-15 11:56:02 +0200

    feat: nixify

--- init.lua
+++ init.lua
@@ -804,6 +804,7 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettier', 'prettierd', stop_after_first = true },
         typescriptreact = { 'prettier', 'prettierd', stop_after_first = true },
+        nix = {'alejandra'}
       },
     },
   },

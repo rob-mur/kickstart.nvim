4325114dec4ac12f27d68e967bec8eda0caf1528
Rob Murphy <robert.murphy@descartesunderwriting.com>
2025-09-09 13:56:15 +0200

    chore: keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,3 +1,4 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = 'Paste from system clipboard' })
+vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { desc = 'Paste from system clipboard (before)' })
 vim.keymap.set({ 'n', 'v' }, '<leader>d', '"+d', { desc = 'Cut to system clipboard' })
